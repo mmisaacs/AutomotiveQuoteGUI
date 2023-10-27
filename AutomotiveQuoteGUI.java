612036c1855e4fe72b9da3f741cf90e3e283199e
Maddie Isaacs <misaacs@cpp.edu>
2023-10-27 01:44:24 -0700

    Add action listener to button and checkboxes

--- AutomotiveQuoteGUI.java
+++ AutomotiveQuoteGUI.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AutomotiveQuoteGUI {
+public class AutomotiveQuoteGUI implements ActionListener {
     JCheckBox oilChange, lubeJob, radiatorFlush, transmission, inspection,
-            muffler, rotation, labor;
-    JTextField numOfHours, partsPrice;
+            muffler, rotation, labor, parts;
+    JTextField numOfHours, partsPrice, totalNum;
+
+    JButton totalPrice;
+
+    double total;
+
 
     AutomotiveQuoteGUI(){
         //create the frame
@@ -13,20 +20,39 @@ public class AutomotiveQuoteGUI {
         jfrm.setLayout(new FlowLayout());
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel routine = new JLabel("Regular Routine Services");
+        JLabel routine = new JLabel("<html><B>Regular Routine Services");
         oilChange = new JCheckBox("Oil Change - $36.00");
-        oilChange.setHorizontalAlignment(JCheckBox.LEFT);
+        oilChange.addActionListener(this);
         lubeJob = new JCheckBox("Lube Job - $18.00");
+        lubeJob.addActionListener(this);
         radiatorFlush = new JCheckBox("Radiator Flush - $30.00");
+        radiatorFlush.addActionListener(this);
         inspection = new JCheckBox("Inspection - $15.00");
+        inspection.addActionListener(this);
         muffler = new JCheckBox("Muffler Replacement - $100.00");
+        muffler.addActionListener(this);
         rotation = new JCheckBox("Tire Rotation - $20.00");
-        labor = new JCheckBox("Labor - 20.00 x per hour: ");
-        JLabel nonRoutine = new JLabel("Non-Routine Services");
-        JLabel parts = new JLabel("Parts(in $): ");
-        partsPrice = new JTextField(5);
+        rotation.addActionListener(this);
 
+        JLabel nonRoutine = new JLabel("<html><B>Non-Routine Services");
+
+        //parts, user fill in value, added to total. editable changes when pressed
+        parts = new JCheckBox("Parts(in $): ");
+        partsPrice = new JTextField(10);
+        partsPrice.setEditable(false);
+        parts.addActionListener(this);
+
+        labor = new JCheckBox("Labor - 20.00 x per hour: ");
         numOfHours = new JTextField(2);
+        numOfHours.setEditable(false);
+        labor.addActionListener(this);
+
+        totalPrice = new JButton("Total");
+        totalPrice.addActionListener(this);
+
+        //JLabel totalPrice = new JLabel("Total Price: $" + total);
+        totalNum = new JTextField(5);
+        totalNum.setEditable(false);
 
         jfrm.getContentPane().add(routine);
         jfrm.getContentPane().add(oilChange);
@@ -40,9 +66,60 @@ public class AutomotiveQuoteGUI {
         jfrm.getContentPane().add(numOfHours);
         jfrm.getContentPane().add(parts);
         jfrm.getContentPane().add(partsPrice);
+        jfrm.getContentPane().add(totalPrice);
+        jfrm.getContentPane().add(totalNum);
 
 
         jfrm.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        total = 0.0;
+        double hours = 0.0;
+        //add routine prices to total if pressed
+        if(oilChange.isSelected()){
+            total += 36.0;
+        }
+        if(lubeJob.isSelected()){
+            total += 18.00;
+        }
+        if(radiatorFlush.isSelected()){
+            total += 30.00;
+        }
+        if(inspection.isSelected()){
+            total += 15.00;
+        }
+        if(muffler.isSelected()){
+            total += 100.00;
+        }
+        if(rotation.isSelected()){
+            total += 20.00;
+        }
+
+        if(labor.isSelected()){
+            numOfHours.setEditable(true);
+            hours = Double.parseDouble(numOfHours.getText());
+        }
+        if(!labor.isSelected()){
+            numOfHours.setEditable(false);
+        }
+        if(parts.isSelected()){
+            partsPrice.setEditable(true);
+            total = Double.parseDouble(partsPrice.getText());
+
+        }
+        //make uneditable if not selected
+        if(!parts.isSelected()){
+            partsPrice.setEditable(false);
+        }
+
+        //if button is pressed, total is outputted
+        if(e.getActionCommand().equals("Total")){
+            total += hours * 20;
+            totalNum.setText("$" + total);
+            total = 0;
+        }
     }
 
     public static void main(String[] args){
